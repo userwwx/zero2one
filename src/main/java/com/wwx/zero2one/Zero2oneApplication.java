6d86bbef1b2287b5bb4bd39d6e3ef71a19cbeda4
userwwx <429101060@qq.com>
2022-02-05 17:01:47 +0800

    Initial commit

---
+++ src/main/java/com/wwx/zero2one/Zero2oneApplication.java
@@ -0,0 +1,13 @@
+package com.wwx.zero2one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zero2oneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zero2oneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wwx/zero2one/Zero2oneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wwx.zero2one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zero2oneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
